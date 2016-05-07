e3952693dfa64b7d1ecb8f5ba44d67720527ad9c
humbbetao <humberto_voleibol@hotmail.com>
2016-05-07 20:31:25 -0300

    Fiz os metodos de bloqueio, mas tem q melhorar a fila e a lista

--- src/bloqueiobifasico/Escalonador.java
+++ src/bloqueiobifasico/Escalonador.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 /**
  *
@@ -21,9 +20,12 @@ import java.util.Queue;
  */
 public class Escalonador {
 
-    PriorityQueue<String> filaDeTransacao;
     ArrayList<String> listaDeTransacao;
     ArrayList<String> dados;
+    PriorityQueue<String> filaDeTransacao;
+    HashMap<String, Integer> componFnteDaLista;
+//    PriorityQueue<HashMap<Integer, ArrayList<String>>> filaDeTransacao;
+    //
     HashMap<String, StatusDoDado> estadoDoDadoCorrente;
 
     public static final String statusDoDadoDesbloqueado = "U";
@@ -53,8 +55,8 @@ public class Escalonador {
             numeroDeLinhas++;
             System.out.println(linha);
             if (numeroDeLinhas == 1) {
-                s = linha.split(" ");
-                numeroDeItensDeDados = s.length;;
+                s = linha.split(", ");
+                numeroDeItensDeDados = s.length;
                 numeroDeTransacoes = Integer.parseInt(s[1]);
                 numeroDeAcessos = Integer.parseInt(s[2]);
             } else if (numeroDeLinhas == 2) {
@@ -77,16 +79,56 @@ public class Escalonador {
         escalonar(schedule);
     }
 
-    public void solicitacaoDeBloqueio() {
+    
+    public void despertarFila(String dado)
+    {
+        //procurar por toda a fila quem esta primeiro e atender o chamado, executa;
+    }
+    
+    public void solicitacaoDeDesbloqueio(String transacao, String dado) {
+        if (estadoDoDadoCorrente.get(dado).equals(statusDoDadoBloqueadoExclusivo)) {
+            estadoDoDadoCorrente.get(dado).setStatus(0);//desbloqueia
+            //desperta a fila-wait(dado);
+//            estadoDoDadoCorrente.get(dado).setStatus(1);
+        } else if (estadoDoDadoCorrente.get(dado).equals(statusDoDadoBloqueadoCompartilhado)) {
+//            listaDeTransacao.add(transacao);
+            listaDeTransacao.remove(dado);
+            if (listaDeTransacao.isEmpty()) {
+                estadoDoDadoCorrente.get(dado).setStatus(0);
+                //desperta a fila-wait(dado);
+            }
+
+        }
+
     }
 
-    public void solicitacaoDeDesbloqueio() {
+    public void solicitacaoDeBloqueioCompartilhado(String transacao, String dado) {
+        if (estadoDoDadoCorrente.get(dado).equals(statusDoDadoDesbloqueado)) {
+            listaDeTransacao.add(transacao);
+            estadoDoDadoCorrente.get(dado).setStatus(1);
+        } else if (estadoDoDadoCorrente.get(dado).equals(statusDoDadoBloqueadoCompartilhado)) {
+            listaDeTransacao.add(transacao);
+        } else {
+            filaDeTransacao.add(transacao);
+        }
+
     }
 
-    public void solicitacaoDeBloqueioCompartilhado() {
+    public void solicitacaoDeBloqueioExclusivo(String transacao, String dado) {
+        if (estadoDoDadoCorrente.get(dado).equals(statusDoDadoDesbloqueado)) {
+            listaDeTransacao.add(transacao);
+            estadoDoDadoCorrente.get(dado).setStatus(2);
+        } else {
+            filaDeTransacao.add(transacao);
+        }
     }
 
-    public void solicitacaoDeBloqueioExclusivo() {
+    public void solicitacaoDeBloqueio(String status, String transacao, String dado) {
+        if (status.equals(statusDoDadoBloqueadoCompartilhado)) {
+            solicitacaoDeBloqueioCompartilhado(transacao, dado);
+        } else {
+            solicitacaoDeBloqueioExclusivo(transacao, dado);
+        }
     }
 
     private void escalonar(String[] schedule) {
@@ -96,8 +138,25 @@ public class Escalonador {
         for (String i : schedule) {
             //verifica se comeca uma transacao;
             if (i.substring(0, 1).equals("S")) {
-                
+                //comeca transacao
             }
+            if (i.substring(0, 1).equals("S")) {
+                //termina transacao
+            }
+            if (i.substring(0, 1).equals("R")) {
+                //solicita bloqueio compartilhado
+                System.out.println(i.substring(1, 2));
+                System.out.println(i.substring(3, 4));
+                solicitacaoDeBloqueio(statusDoDadoBloqueadoCompartilhado, i.substring(1, 2), i.substring(4, 5));
+            }
+            if (i.substring(0, 1).equals("W")) {
+                //solicita bloqueio exclusivo
+                System.out.println(i.substring(1, 2));
+                System.out.println(i.substring(3, 4));
+                solicitacaoDeBloqueio(statusDoDadoBloqueadoExclusivo, i.substring(1, 2), i.substring(4, 5));
+
+            }
+
         }
     }
 
--- src/bloqueiobifasico/StatusDoDado.java
+++ src/bloqueiobifasico/StatusDoDado.java
@@ -31,4 +31,5 @@ public enum StatusDoDado {
         this.status = status;
     }
 
+
 }
